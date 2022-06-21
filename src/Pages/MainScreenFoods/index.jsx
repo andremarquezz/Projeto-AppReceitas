5d7445096f85b63396e212e988eb8c796647f0f4
diegotimao <diegotimao1104@gmail.com>
2022-06-21 15:35:01 -0500

    Estilização página MainScreenFoods

--- src/Pages/MainScreenFoods/index.jsx
+++ src/Pages/MainScreenFoods/index.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
 
+import './index.css';
+
+const name = 'Chelsea Buns';
+
 function MainScreenFoods() {
-  return <div>MainScreenFoods</div>;
+  return (
+    <div className="main-foods-container">
+      <div className="main-foods-content">
+        <div className="filters-button">
+          <button type="button" className="btn-filter">All</button>
+          <button type="button" className="btn-filter">Beef</button>
+          <button type="button" className="btn-filter">Lamb</button>
+          <button type="button" className="btn-filter">Chicken</button>
+          <button type="button" className="btn-filter">Breakfast</button>
+          <button type="button" className="btn-filter">Dessert</button>
+        </div>
+        <ul className="foods-list">
+          <li className="foods-list-item">
+            <img
+              src="https://media-cdn.tripadvisor.com/media/photo-s/10/84/21/e8/the-chelsea-bun.jpg"
+              alt="FoodsImage"
+            />
+            <strong>{ name }</strong>
+          </li>
+
+          <li className="foods-list-item">
+            <img
+              src="https://media-cdn.tripadvisor.com/media/photo-s/10/84/21/e8/the-chelsea-bun.jpg"
+              alt="FoodsImage"
+            />
+            <strong>{ name }</strong>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default MainScreenFoods;
