de8061046edfb9c327f03efa590365ee3e3d3685
jey <andre.marqquez@gmail.com>
2022-06-24 15:15:53 -0300

    fix: merge

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { actionRadioFilter, actionTextFilter } from '../../redux/slices/filterSlice';
+import './index.css';
 
 export default function Search({ inputText }) {
   const dispatch = useDispatch();
@@ -19,7 +20,6 @@ export default function Search({ inputText }) {
   return (
     <div className="search-container">
       <div className="search-content">
-
         <form onSubmit={ (e) => handleFilter(e) }>
           <label htmlFor="ingredient">
             Ingredient
@@ -57,28 +57,12 @@ export default function Search({ inputText }) {
               onClick={ ({ target }) => setValueRadio(target.value) }
             />
           </label>
-        <label htmlFor="letter">
-          First letter
-          <input
-            type="radio"
-            id="letter"
-            value="letter"
-            name="filter-type"
-            data-testid="first-letter-search-radio"
-            onClick={ ({ target }) => setValueRadio(target.value) }
-          />
-        </label>
-      </div>
-      <div className="search-btn">
-        <button
-          className="btn-search"
-          type="submit"
-          data-testid="exec-search-btn"
-        >
-          Search
-        </button>
+          <div className="search-btn">
+            <button className="btn-search" type="submit" data-testid="exec-search-btn">
+              Search
+            </button>
+          </div>
         </form>
-
       </div>
     </div>
   );
