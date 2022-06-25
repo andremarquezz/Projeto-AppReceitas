b38e93b728f45427b3a820adb1b0a0e6a4f8f08c
jey <andre.marqquez@gmail.com>
2022-06-25 07:37:50 -0300

    feat: adiciona footer nas paginas necessarias

--- src/Pages/ScreenProfile/index.jsx
+++ src/Pages/ScreenProfile/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 
 function ScreenProfile() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
