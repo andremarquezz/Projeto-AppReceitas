4e7908754769bb64a5288b68da598ee4e8698f6b
jey <andre.marqquez@gmail.com>
2022-06-28 18:06:42 -0300

    resolve problema do btn

--- src/components/ListRecipies/index.jsx
+++ src/components/ListRecipies/index.jsx
@@ -29,13 +29,12 @@ function ListRecipies() {
   }, [pathname]);
 
   const btnFilterCategory = (category) => {
-    const cate = category.replace(' ', '_');
     if (lastCategory === category) {
       dispatch(actionCardCategories(false));
       setlastCategory('');
     } else {
       const type = pathname === '/foods' ? 'meals' : 'drinks';
-      categoryFilter(cate, type)
+      categoryFilter(category, type)
         .then((response) => dispatch(actionCategoryFilter(response)));
       setlastCategory(category);
 
