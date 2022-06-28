c7fa248bd20c5dfebe6e44985d55790d016e0473
jey <andre.marqquez@gmail.com>
2022-06-28 18:17:42 -0300

    fix: remove logs

--- src/services/apiFilter.js
+++ src/services/apiFilter.js
@@ -54,7 +54,7 @@ export const categoryFilter = (category, type) => {
       `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`,
     );
   default:
-    return console.log('default category');
+    return '404 categoryFilter';
   }
 };
 
