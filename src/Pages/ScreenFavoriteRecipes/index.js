346a1359b5ea3d3d9a9b5330ebbb1d05b9078940
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-07-04 01:04:32 -0300

    adiciona a logica da pagina DoneRecipes para o componente DoneAndFavorites que agora exibira a logica do Done e do FavoriteRecipe. requisitos do 54 ao 60 estão passando

--- src/Pages/ScreenFavoriteRecipes/index.js
+++ src/Pages/ScreenFavoriteRecipes/index.js
@@ -2,28 +2,29 @@ import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header';
 import DoneAndFavorite from '../../components/DoneAndFavorite';
 import { getLocalStorage } from '../../services/LocalStorage';
+// import FavoritesCard from '../../components/FavoritesCard';
 
 import './index.css';
 import atentionIcon from '../../images/atencao.svg';
 
 function ScreenFavoriteRecipes() {
-  const [haveDone, setHaveDone] = useState(false);
+  const [haveFavotire, setHaveFavorite] = useState(false);
   useEffect(() => {
     const favoriteStorage = async () => {
-      const favoriteCheck = await getLocalStorage('doneRecipes');
-      if (favoriteCheck) { setHaveDone(true); }
+      const favoriteCheck = await getLocalStorage('favoriteRecipes');
+      if (favoriteCheck) { setHaveFavorite(true); }
     };
     favoriteStorage();
   }, []);
   return (
     <div className="favorites-container">
       <Header />
-      {haveDone ? (<DoneAndFavorite />
+      {haveFavotire ? (<DoneAndFavorite />
       ) : (
         <div className="favorites-not-found-container">
           <div className="favorites-not-found">
             <img src={ atentionIcon } alt="AtençãoIcon" />
-            <h3>Você ainda não possui receitas completas.</h3>
+            <h3>Você ainda não possui cards como favoritos.</h3>
           </div>
         </div>)}
     </div>
---
+++ src/components/DoneAndFavorite/index.js
@@ -0,0 +1,154 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import './index.css';
+import shareIcon from '../../images/shareIcon.svg';
+import { getDoneRecipes } from '../../services/doneRecipes';
+import { getLocalStorage } from '../../services/LocalStorage';
+
+const copy = require('clipboard-copy');
+
+const NO_COPY = -1;
+const checkLocation = (path) => path.split('/')[1];
+
+export default function DoneAndFavorite() {
+  const [filter, setFilter] = useState('all');
+  const [copied, setCopied] = useState(NO_COPY);
+  const [displayMessage, setDisplayMessage] = useState('');
+  const [myRecipes, setMyRecipes] = useState([]);
+
+  const { location: { pathname } } = useHistory();
+  const myLocation = checkLocation(pathname);
+
+  // const recipeStorage = myLocation === 'favorite-recipes'
+  //   ? getLocalStorage('favoriteRecipes') : getDoneRecipes();
+  // const getRecipes = recipeStorage || [];
+
+  // const myRecipes = getRecipes.filter(
+  //   (recipe) => filter === 'all' || recipe.type === filter,
+  // );
+  console.log(myRecipes);
+
+  useEffect(() => {
+    const recipeStorage = myLocation === 'favorite-recipes'
+      ? getLocalStorage('favoriteRecipes') : getDoneRecipes();
+    if (recipeStorage) {
+      const recipeFiltered = recipeStorage.filter(
+        (recipe) => filter === 'all' || recipe.type === filter,
+      );
+      setMyRecipes(recipeFiltered);
+    }
+  }, [myLocation, filter]);
+
+  const copiedOnScreenTimer = (id) => {
+    const TEXT_TIMER = 5000;
+    setCopied(id);
+    const textTimeout = setTimeout(() => {
+      setCopied(NO_COPY);
+      clearTimeout(textTimeout);
+    }, TEXT_TIMER);
+  };
+
+  useEffect(() => {
+    if (myLocation === 'favorite-recipes') { setDisplayMessage('Receitas Favoritas'); }
+    if (myLocation === 'done-recipes') { setDisplayMessage('Receitas Concluídas'); }
+  }, [myLocation]);
+
+  const copyToClipboard = (type, id) => {
+    copy(`http://localhost:3000/${type}/${id}`);
+    copiedOnScreenTimer(id);
+  };
+
+  // ====//====RETURN====//====//
+  return (
+    <div>
+      <div className="done-description-filter">
+        <strong>Filtros</strong>
+      </div>
+      <div className="filters-done">
+        <button
+          type="button"
+          data-testid="filter-by-all-btn"
+          onClick={ () => setFilter('all') }
+        >
+          All
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-food-btn"
+          onClick={ () => setFilter('food') }
+        >
+          Foods
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-drink-btn"
+          onClick={ () => setFilter('drink') }
+        >
+          Drinks
+        </button>
+      </div>
+      <div className="done-description-items">
+        <strong>{ displayMessage }</strong>
+      </div>
+      <div className="card-done-content">
+        {myRecipes.map((recipe, index) => (
+          <>
+            <div key={ index + recipe } className="done-recipies-item">
+              <div className="done-card-img">
+                <Link to={ `/${recipe.type}s/${recipe.id}` }>
+                  <img
+                    src={ recipe.image }
+                    alt="icon"
+                    data-testid={ `${index}-horizontal-image` }
+                  />
+                </Link>
+              </div>
+              <div className="done-card-body">
+                <div className="card-body-header">
+                  <p data-testid={ `${index}-horizontal-top-text` }>
+                    {recipe.type === 'food'
+                      ? `${recipe.nationality} - ${recipe.category}`
+                      : recipe.alcoholicOrNot}
+                  </p>
+                  {recipe.doneDate
+                    && (
+                      <strong data-testid={ `${index}-horizontal-done-date` }>
+                        { recipe.doneDate }
+                      </strong>)}
+                </div>
+                <Link to={ `/${recipe.type}s/${recipe.id}` }>
+                  <p
+                    data-testid={ `${index}-horizontal-name` }
+                    className="horizontal-name"
+                  >
+                    {recipe.name}
+                  </p>
+                </Link>
+                <div className="done-action">
+                  <div className="tags">
+                    {recipe.tags && recipe.tags.map((tag, i) => (
+                      <p key={ tag + i } data-testid={ `${index}-${tag}-horizontal-tag` }>
+                        {tag}
+                      </p>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="btn-share">
+              {copied === recipe.id && <strong>Link copied!</strong>}
+              <input
+                type="button"
+                className="btn-share"
+                onClick={ () => copyToClipboard(`${recipe.type}s`, recipe.id) }
+                data-testid={ `${index}-horizontal-share-btn` }
+                src={ shareIcon }
+                alt="IconShare"
+              />
+            </div>
+          </>
+        ))}
+      </div>
+    </div>
+  );
+}
