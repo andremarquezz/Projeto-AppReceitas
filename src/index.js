198140563898cf431449a235a759abb7176998f9
jey <andre.marqquez@gmail.com>
2022-07-06 22:10:06 -0300

    feat: deploy gh pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
@@ -8,11 +8,11 @@ import store from './redux/store';
 import './App.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={ store }>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
 
   document.getElementById('root'),
 );
